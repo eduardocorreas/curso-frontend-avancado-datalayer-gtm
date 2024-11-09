1069c2ea7f8aa0541492d8b14f0c8b8518817853
Eduardo Correa <luiz.correa@gran.com>
2024-11-09 01:21:00 -0300

    Adiciona rastreamento de cliques e visualizações no componente PostPreview

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { type Author } from "@/interfaces/author";
 import Link from "next/link";
 import Avatar from "./avatar";
 import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
+import { sendToDatalayer } from "../utils";
 
 type Props = {
   title: string;
@@ -21,13 +23,19 @@ export function PostPreview({
   author,
   slug,
 }: Props) {
+
+  function handleLinkClick() {
+    sendToDatalayer('post_click', 'click', 'post', title);
+    sendToDatalayer('post_viewed', 'view', 'post', title);
+  }
+
   return (
     <div>
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`/posts/${slug}`} className="hover:underline">
+        <Link onClick={handleLinkClick} href={`/posts/${slug}`} target="_blank" className="hover:underline">
           {title}
         </Link>
       </h3>
